f19b2068f4861d0df7369b120b0c6e3a552fad8c
Hector Diaz <Hectrdiaz@gmail.com>
2023-08-08 13:27:01 -0700

    created calc structure and rough functionality

---
+++ calcJS2.js
@@ -0,0 +1,24 @@
+//event listener grabs value and places it in input window 
+//event listener grabs operator when operator buttons are pushed 
+//function adds operator to existing values (a + +)
+//event lister grabs new value and adds it to existing expression (a + b)
+//event listener grabs '=' signs and completes express
+//document.querySelector returns value to input window
+
+
+const buttonInput = document.querySelectorAll('li');  
+
+buttonInput.forEach(button =>{
+    button.addEventListener('click', grab);
+})
+
+// const display = document.getElementById('screen', calculate)
+// const buttons = document.querySelectorAll('button')
+
+// let calculation = []  
+
+function grab(event) {
+    const value = event.target.textContent;
+    document.querySelector('input').innerText = value
+    // console.log(clickedItemValue)  
+}
