62b815d92be71a2194b75ca62bf0487640ba9e30
hectorHumanDev <113396173+MF-H0d@users.noreply.github.com>
2023-08-31 20:43:44 -0700

    code is more dry and efficient

--- calcJS2.js
+++ calcJS2.js
@@ -3,21 +3,43 @@ let listOfOperatorButtons = document.querySelectorAll('.button[data-type=operato
 let calcDisplay = document.querySelector('.calculator_display')
 
 
-// const operators = ()
+
+// const operators = {
+//     add: function(n1, n2) {
+//         return n1 + n2
+//     }
+//     subtract: function(n1, n2){
+//         return n1 - n2
+//     }
+//     multiply: function(n1, n2){
+//         return n1 * n2
+//     }
+//     divide: function(n1, n2){
+//         return n1 / n2
+//     }
+// }
 
 for (let i = 0; i < listOfNumButtons; i++) {
 
-    document.querySelectorAll('.button[data-type=number]')[i].addEventListener('click', function() {
-        let currentValue = this.textContent
-        document.querySelector('.calculator_display').innerHTML = currentValue
+    document.querySelectorAll('.button[data-type=number]')[i].addEventListener('click', function () {
+        const currentValue = calcDisplay.textContent
+        const eventValue = this.textContent
+        let newValue = currentValue === '0' ? eventValue : currentValue + eventValue
+
+       
+
+        calcDisplay.innerHTML = newValue
+
+
+
     })
 }
 
 for (let i = 0; i < listOfOperatorButtons; i++) {
 
-    document.querySelectorAll('.button[data-type=operator]')[i].addEventListener('click', function() {
-        let currentValue = this.textContent 
-        document.querySelector('.calculator_display').innerHTML = currentValue
+    document.querySelectorAll('.button[data-type=operator]')[i].addEventListener('click', function () {
+        let eventValue = this.textContent
+        document.querySelector('.calculator_display').innerHTML = eventValue
     })
 }
 
