d35712af3bfbf826c216cc500df3da0393aa7fe8
hectorHumanDev <113396173+MF-H0d@users.noreply.github.com>
2023-09-05 16:07:28 -0700

    experimented with adding classes via JS event listeners

--- calcJS2.js
+++ calcJS2.js
@@ -4,43 +4,47 @@ let calcDisplay = document.querySelector('.calculator_display')
 
 
 
-// const operators = {
-//     add: function(n1, n2) {
-//         return n1 + n2
-//     }
-//     subtract: function(n1, n2){
-//         return n1 - n2
-//     }
-//     multiply: function(n1, n2){
-//         return n1 * n2
-//     }
-//     divide: function(n1, n2){
-//         return n1 / n2
-//     }
-// }
+const operators = {
+    add: function(n1, n2) {
+        return this.n1 + this.n2
+    },
+    sub: function(n1, n2){
+        return this.n1 - this.n2
+    },
+    mult: function(n1, n2){
+        return this.n1 * this.n2
+    },
+    div: function(n1, n2){
+        return this.n1 / this.n2
+    },
+}
 
 for (let i = 0; i < listOfNumButtons; i++) {
-
+    
     document.querySelectorAll('.button[data-type=number]')[i].addEventListener('click', function () {
-        const currentValue = calcDisplay.textContent
-        const eventValue = this.textContent
-        let newValue = currentValue === '0' ? eventValue : currentValue + eventValue
-
-       
-
-        calcDisplay.innerHTML = newValue
-
-
-
+        const currentValue = calcDisplay.textContent;
+        const eventValue = this.textContent;
+        let newValue = currentValue === '0' ? eventValue : currentValue + eventValue;
+        calcDisplay.innerHTML = newValue;
     })
 }
+
 
 for (let i = 0; i < listOfOperatorButtons; i++) {
 
     document.querySelectorAll('.button[data-type=operator]')[i].addEventListener('click', function () {
-        let eventValue = this.textContent
+        
+        
+        currentValue = calcDisplay.textContent;
+        let eventValue = this.textContent;
         document.querySelector('.calculator_display').innerHTML = eventValue
     })
+}
+
+if(calcDisplay.textContent.length > 14){
+    calcDisplay = calcDisplay.classList.add('calculator_display2')
+}else{
+    calcDisplay
 }
 
 
