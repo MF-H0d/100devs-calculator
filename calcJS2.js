a4d65953bf77f08a34f72892ef6f402d190953cc
hectorHumanDev <113396173+MF-H0d@users.noreply.github.com>
2023-09-26 20:58:43 -0700

    improved calc functionality

--- calcJS2.js
+++ calcJS2.js
@@ -1,51 +1,72 @@
-let listOfNumButtons = document.querySelectorAll('.button[data-type=number]').length
-let listOfOperatorButtons = document.querySelectorAll('.button[data-type=operator]').length
+let listOfNumButtons = document.querySelectorAll('.button[data-type=number]')
+let listOfOperatorButtons = document.querySelectorAll('.op_btn')
 let calcDisplay = document.querySelector('.calculator_display')
+let firstVal = calcDisplay.textContent
+let operator
+let secondValue
 
+for (let i = 0; i < listOfNumButtons.length; i++) {
 
-
-const operators = {
-    add: function(n1, n2) {
-        return this.n1 + this.n2
-    },
-    sub: function(n1, n2){
-        return this.n1 - this.n2
-    },
-    mult: function(n1, n2){
-        return this.n1 * this.n2
-    },
-    div: function(n1, n2){
-        return this.n1 / this.n2
-    },
-}
-
-for (let i = 0; i < listOfNumButtons; i++) {
-    
-    document.querySelectorAll('.button[data-type=number]')[i].addEventListener('click', function () {
-        const currentValue = calcDisplay.textContent;
+    listOfNumButtons[i].addEventListener('click', function () {
         const eventValue = this.textContent;
-        let newValue = currentValue === '0' ? eventValue : currentValue + eventValue;
-        calcDisplay.innerHTML = newValue;
+        let newValue
+        if (operator) {
+            newValue = secondValue ? secondValue + eventValue : eventValue
+            secondValue = newValue
+        } else {
+            newValue = firstVal === '0' ? eventValue : firstVal + eventValue;
+            firstVal = newValue
+        }
+        calcDisplay.innerHTML = newValue
     })
 }
 
 
-for (let i = 0; i < listOfOperatorButtons; i++) {
+for (let i = 0; i < listOfOperatorButtons.length; i++) {
 
-    document.querySelectorAll('.button[data-type=operator]')[i].addEventListener('click', function () {
-        
-        
-        currentValue = calcDisplay.textContent;
-        let eventValue = this.textContent;
-        document.querySelector('.calculator_display').innerHTML = eventValue
+    listOfOperatorButtons[i].addEventListener('click', function (event) {
+        operator = event.target.textContent
+        console.log(operator)
+        calcDisplay.innerHTML = ""
+
+//operator buttons printing NaN to calc screen..create separate '.btn' class that is tied directly to button elements of only the operator buttons
+
+        switch (operator) {
+                case '/':
+                    return firstVal / secondValue
+                    break;
+                case 'x':
+                    return firstVal * secondValue
+                    break
+                case '+':
+                    return firstVal + secondValue
+                    break
+                case '-':
+                    return firstVal - secondValue
+                    break
+                default:
+                    break;
+            }
     })
 }
 
-if(calcDisplay.textContent.length > 14){
-    calcDisplay = calcDisplay.classList.add('calculator_display2')
-}else{
-    calcDisplay
-}
+document.querySelector('.button[data-type=equals]').addEventListener('click', function (event){
+    calcDisplay.innerHTML = eval(firstVal + operator + secondValue)
+})
+
+
+
+
+
+
+
+
+// if(calcDisplay.textContent.length > 14){
+//     calcDisplay = calcDisplay.classList.add('calculator_display2')
+// }else{
+//     calcDisplay
+// }
+
 
 
 
